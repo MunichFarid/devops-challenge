9a981cd7fbf88eefbe8a8bf8b5af202f8baf5dd4
Farid <munichfarid@gmail.com>
2025-10-02 17:45:36 +0200

    Fixing registry name

--- kind-load.sh
+++ kind-load.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-DIGEST=$(docker manifest inspect ghcr.io/munichfarid/fastapi-test:latest \
+DIGEST=$(docker manifest inspect ghcr.io/munichfarid/devops-challenge:latest \
   | jq -r '.manifests[] | select(.platform.os=="linux" and .platform.architecture=="amd64") | .digest' | head -n1)
 
 echo "$DIGEST"
-docker pull ghcr.io/munichfarid/fastapi-test@${DIGEST}
-docker tag ghcr.io/munichfarid/fastapi-test@${DIGEST} ghcr.io/munichfarid/fastapi-test:amd64-latest
-kind load docker-image ghcr.io/munichfarid/fastapi-test:amd64-latest --name kind
+docker pull ghcr.io/munichfarid/devops-challenge@${DIGEST}
+docker tag ghcr.io/munichfarid/devops-challenge@${DIGEST} ghcr.io/munichfarid/devops-challenge:amd64-latest
+kind load docker-image ghcr.io/munichfarid/devops-challenge:amd64-latest --name kind
